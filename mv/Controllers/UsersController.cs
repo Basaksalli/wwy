a72bac8f3dcfecde0a1ac49e1c21310c24bd6ad2
Cem Özkılıç <cemmozkilic@gmail.com>
2016-02-17 02:26:45 +0200

    kullanıcı düzenleme

--- mv/Controllers/UsersController.cs
+++ mv/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using mv.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -21,6 +22,7 @@ namespace mv.Controllers
 
         public ActionResult Login()
         {
+            ViewData["error"] = TempData["error"];
             return View();
         }
 
@@ -30,8 +32,19 @@ namespace mv.Controllers
             var kullanici = (from item in ent.Users
                              where item.Email == u.Email && item.Password == u.Password
                              select item).FirstOrDefault();
-            Session["KullaniciLogin"] = kullanici;
-            return RedirectToAction("Index", "ProfilePage");
+
+            if (kullanici!=null)
+            { Session["KullaniciLogin"] = kullanici;
+                return RedirectToAction("Index", "ProfilePage");
+            }
+            else
+            {
+                TempData["error"] = "Kullanici Bilgileri Hatalı..!";
+                return View();
+
+                //Kullanıcı bilgileri hatalı mesajı verilmesi gereklidir.
+                
+            }
         }
 
         public ActionResult Register()
@@ -80,6 +93,45 @@ namespace mv.Controllers
 
         }
 
+        public ActionResult Edit(int id)
+        {
+            var user = (from item in ent.Users
+                        where item.ID == id
+                        select item).FirstOrDefault();
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult Edit(Users user)
+        {
+            //değişek olan kayda odaklandık.
+            var degisen = (from item in ent.Users
+                     where item.ID == user.ID
+                     select item).FirstOrDefault();
 
+            //odaklandımığız nesnenin propertylerine müdahale edityoruz.
+
+            degisen.Email = user.Email;
+            degisen.Password = user.Password;
+            degisen.Name = user.Name;
+            degisen.Surname = user.Surname;
+            degisen.Phone = user.Phone;
+            degisen.Country = user.Country;
+            degisen.City = user.City;
+            degisen.Birthdate = user.Birthdate; 
+            degisen.PictureLoc = user.PictureLoc;
+            try
+            {
+                ent.SaveChanges();
+                return Redirect("http://localhost:22572/ProfilePage");
+                
+            }
+            catch (SqlException ex)
+            {
+
+                string hata = ex.Message;
+                return View(user);
+
+            }
+        }
     }
 }
